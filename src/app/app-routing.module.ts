75c5ebc80186f6160b76060f4e5ee5582b9924cd
Nitin Patil <nitinpatil@benchmarkitsolutions.com>
2018-09-03 11:24:47 +0530

    common headerr footer done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import{HeaderComponent} from './header/header.component';
-import{FooterComponent} from './footer/footer.component';
+
 import{HomeComponent} from './home/home.component';
 import{ServicesComponent} from './services/services.component';
 
@@ -9,8 +8,6 @@ const routes: Routes = [
 
   { path: 'home', component: HomeComponent },
   { path: 'services', component: ServicesComponent },
-  { path: 'header', component: HeaderComponent },
-  { path: 'footer', component: FooterComponent },
   {path:'',redirectTo: '/home', pathMatch: 'full'},
 
 ];
@@ -19,8 +16,7 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
   declarations:[
-    HeaderComponent,
-    FooterComponent,
+   
     HomeComponent,
     ServicesComponent
   ],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 import { LocationStrategy, HashLocationStrategy } from '../../node_modules/@angular/common';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+   
   ],
   providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }],
   bootstrap: [AppComponent]
