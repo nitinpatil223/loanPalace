b1ae59d957d475fe194f12081c0278b961bee918
Nitin Patil <nitinpatil583@gmail.com>
2018-09-22 19:40:06 +0530

    products cars

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, Renderer2 } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit { 
 
-  constructor() { }
+  constructor(@Inject(DOCUMENT) private document: Document, private renderer: Renderer2) { }
 
   ngOnInit() {
+    this.renderer.addClass(document.body, 'home-body');
+  }
+
+  ngOnDestroy() {
+    this.renderer.removeClass(document.body, 'home-body');
   }
 
 }
